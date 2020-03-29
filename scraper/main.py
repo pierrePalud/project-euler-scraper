f4192a3461a7e06cf454049ae59c508b0f60ae88
pierrePalud <32699249+pierrePalud@users.noreply.github.com>
2020-03-29 20:23:41 -0300

    exclusion of data and result directories from .gitignore, and automated their creation if necessary

--- scraper/main.py
+++ scraper/main.py
@@ -8,6 +8,10 @@ import constants as c
 
 
 if __name__ == '__main__':
+    for dir_name in [c.DATA_DIR, c.RESULT_DIR]:
+        if not(os.path.isdir(dir_name)):
+            os.mkdir(dir_name)
+        
     print(f"Importation of Project Euler problems data Started on {time.ctime()}.")
     df_euler = scr.scrape_euler_problems_global()
     print("Importation of Project Euler problems data Successful.")
